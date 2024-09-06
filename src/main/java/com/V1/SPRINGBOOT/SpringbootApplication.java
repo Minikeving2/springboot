92556fe0d54c8160ab2bed28a2991429efba0562
portatil kevin <kevingarcia4171@gmail.com>
2024-09-06 16:36:24 -0500

    inicio

---
+++ src/main/java/com/V1/SPRINGBOOT/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.V1.SPRINGBOOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
